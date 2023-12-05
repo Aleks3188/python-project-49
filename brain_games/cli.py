1023ab170dc3f7b3cdeda821786bd06b9f6908fe
Aleks3188 <chernovol_a.v@mail.ru>
2023-12-05 11:31:37 +0300

    update 05/12

---
+++ brain_games/cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import prompt
+
+def welcome_user():
+    print(f"brain-games\nWelcome to the Brain Games!")
+    name_gamer = prompt.string('May I have your name?')
+
+
+
+
+welcome_user() 
