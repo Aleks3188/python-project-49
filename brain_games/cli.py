c3457f2d4fe34bcce8897360f98a517a8cfa1b99
Aleks3188 <chernovol_a.v@mail.ru>
2023-12-05 15:55:12 +0300

    finish step3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,7 @@ import prompt
 def welcome_user():
     print(f"brain-games\nWelcome to the Brain Games!")
     name_gamer = prompt.string('May I have your name? ')
+    print("Hello," + name_gamer)
 
 
 welcome_user() 
