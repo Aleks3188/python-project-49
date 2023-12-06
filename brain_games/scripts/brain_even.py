332019c2a57340268d8049b15d5b7fb6dc087064
Aleks3188 <chernovol_a.v@mail.ru>
2023-12-06 16:55:43 +0300

    update

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,43 @@
 #!/home/admin_ar/hexlet-git/python-project-49/.venv/bin/python
-from brain_games.cli import welcome_user
+from random import randint
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name_gamer = prompt.string('May I have your name? ')
+    print(f"Hello, {name_gamer}")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for index in range(3): 
+        number_random = randint(1, 10) 
+        print(f'Question: {number_random}')
+        geymer__answer = prompt.string('Your answer: ')
+        if number_random % 2 == 0:
+            if geymer__answer == 'yes':
+                print('Correct!')
+            elif geymer__answer == 'no':
+                print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name_gamer}
+            ''')
+                break
+        else: 
+            print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name_gamer}
+            ''') 
+            break
+        if number_random % 2 != 0:
+            if geymer__answer == 'no':
+                print('Correct!')
+            elif geymer__answer == 'yes':
+                print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name_gamer}
+            ''')
+                break
+        else: 
+            print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name_gamer}
+            ''') 
+            break
 
 
 def main():
