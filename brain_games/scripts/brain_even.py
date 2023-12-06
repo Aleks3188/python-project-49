5066e30a3a6b1c01c53c63346ee5d3f6b564d9bf
Aleks3188 <chernovol_a.v@mail.ru>
2023-12-06 22:37:59 +0300

    update brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,7 +8,8 @@ def welcome_user():
     name_gamer = prompt.string('May I have your name? ')
     print(f"Hello, {name_gamer}")
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    for index in range(3): 
+    correct_answer = 0 
+    for index in range(3):
         number_random = randint(1, 10) 
         print(f'Question: {number_random}')
         geymer__answer = prompt.string('Your answer: ')
@@ -16,16 +17,11 @@ def welcome_user():
             if geymer__answer == 'yes':
                 print('Correct!')
             elif geymer__answer == 'no':
-                print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+                print(f''''no' is wrong answer ;(. Correct answer was 'yes'.
 Let's try again, {name_gamer}
             ''')
                 break
-        else: 
-            print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
-Let's try again, {name_gamer}
-            ''') 
-            break
-        if number_random % 2 != 0:
+        elif number_random % 2 != 0:
             if geymer__answer == 'no':
                 print('Correct!')
             elif geymer__answer == 'yes':
@@ -34,10 +30,13 @@ Let's try again, {name_gamer}
             ''')
                 break
         else: 
-            print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+            print(f'''{geymer__answer} is wrong answer ;(. Correct answer was 'no'.
 Let's try again, {name_gamer}
             ''') 
             break
+        correct_answer += 1
+    if correct_answer == 3:
+        print(f'Congratulations, {name_gamer}')
 
 
 def main():
