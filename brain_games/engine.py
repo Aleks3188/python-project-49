e1d5e83a3aa74dd4764c80f04908f7c2ced87c1b
Aleks3188 <chernovol_a.v@mail.ru>
2023-12-08 15:42:15 +0300

    update brain-eve

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,4 +8,16 @@ GAME_ROUNDS = 3
 def run_games(game):
     name_gamer = welcome_user()
     print(game.DESCRIPTION)
-
+    for _ in range(GAME_ROUNDS):
+        gener_number, gener_answer = game.gener_ques_and_answer()
+        print(f'Question: {gener_number}')
+        gamer_answer = prompt.string('Your answer: ')
+        if gamer_answer != gener_answer:
+            print(
+                f"'{gamer_answer}' is wrong answer ;(."
+                f"Correct answer was '{gener_answer}'"
+                )
+            print(f"Let's try again, {name_gamer}")
+            return
+        print('Correct!')
+    print(f'Congratulation, {name_gamer}')
--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,30 +1,9 @@
-import random
+from random import randint
+
 
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-def generation_ques_and_answer():
-    correct_answer = 0
-    # Answers yes or no because lenght_string > 80
-    yes_answer = "is wrong answer ;(. Correct answer was 'yes'."
-    no_answer = "is wrong answer ;(. Correct answer was 'no'."
-    for _ in range(3):
-        number_random = randint(1, 20)
-        print(f'Question: {number_random}')
-        geymer__answer = prompt.string('Your answer: ')
-        if number_random % 2 == 0:
-            if geymer__answer == 'yes':
-                print('Correct!')
-            else:
-                print(f"'{geymer__answer}' {yes_answer}")
-                print(f"Let's try again, {name_gamer}")
-                break
-        else:
-            if geymer__answer == 'no':
-                print('Correct!')
-            else:
-                print(f"'{geymer__answer}' {no_answer}")
-                print(f"Let's try again, {name_gamer}")
-                break
-        correct_answer += 1
-    if correct_answer == 3:
-        print(f'Congratulations, {name_gamer}')
+def gener_ques_and_answer():
+    gener_number = randint(1, 20)
+    gener_answer = 'yes' if gener_number % 2 == 0 else 'no'
+    return gener_number, gener_answer
